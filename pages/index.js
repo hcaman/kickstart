51465405e67d469e9bebe34ba913b2140333a513
Hugo Manzoni <hugocmdesign@gmail.com>
2022-08-11 16:42:02 +0200

    add the routes for component show and new campaign

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Card, Button } from 'semantic-ui-react';
+import { Link } from '../routes';
 import factory from '../ethereum/factory';
 import Layout from '../components/Layout';
 
@@ -13,7 +14,11 @@ class CampaignIndex extends Component {
         const items = this.props.campaigns.map(address => {
             return {
                 header: address,
-                description: <a>View Campaign</a>,
+                description: (
+                    <Link route={`/campaigns/${address}`}>
+                        <a>View Campaign</a>
+                    </Link>
+                ),
                 fluid: true
             }
         });
@@ -26,7 +31,11 @@ class CampaignIndex extends Component {
             <Layout>
                 <div>
                     <h3>Open Campaigns</h3>
-                    <Button floated='right' content='Create Campaign' icon='add' primary />
+                    <Link route='/campaigns/new'>
+                        <a>
+                            <Button floated='right' content='Create Campaign' icon='add' primary />
+                        </a>
+                    </Link>
                     {this.renderCampaigns()}
                 </div>
             </Layout>
