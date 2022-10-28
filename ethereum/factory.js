ed58a7e2a1b067e8c25af80eb90722b322007e3a
Hugo Manzoni <hugocmdesign@gmail.com>
2022-10-28 19:56:16 +0100

    change the env variable

--- ethereum/factory.js
+++ ethereum/factory.js
@@ -1,10 +1,9 @@
 import web3 from './web3';
-import { env } from 'process';
 import CampaignFactory from './build/CampaignFactory.json';
 
 const instance = new web3.eth.Contract(
     CampaignFactory.abi,
-    env.CONTRACT_ADDRESS
+    'CONTRACT_ADDRESS'
 );
 
 export default instance;
